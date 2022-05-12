cb6187f6f23fc3e50c5ae7c5641028c3988e099b
richard y <ryuan2003@gmail.com>
2022-05-12 16:21:14 -0700

    edited navbar, tried to implement navbar

--- edstruments/src/App.js
+++ edstruments/src/App.js
@@ -3,9 +3,11 @@ import ItemTable from './table';
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import Box from '@material-ui/core/Box';
 
 function App() {
   return (
+
     <div className="App">
       <Navbar bg="light" expand="lg">
         <Container>
@@ -13,22 +15,36 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <Nav.Link href="#dashboard">Dashboard</Nav.Link>
+              <Nav.Link href="#resources">Resources</Nav.Link>
+              <Nav.Link href="#budgets">Budgets</Nav.Link>
+              <Nav.Link href="#transactions-pos">Transactions/POs</Nav.Link>
+              <Nav.Link href="#assets">Assets</Nav.Link>
+              <Nav.Link href="#personal">Personal</Nav.Link>
+              <Nav.Link href="#schools">Schools</Nav.Link>
+              {/* Code for implmenting a dropdown menu, if we need it*/}
+              {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
+              </NavDropdown> */}
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
       
-      <h1>Please fill out the form to log your item!</h1>
-      <ItemTable />    
+      <div style={{ marginLeft: '5%', marginTop: '40px', width: '90%' }}>
+        <Box border={1} borderColor="black">
+          BOX GOES HERE
+          <h1>Catalog</h1>
+          <ItemTable />
+          BOX ENDS HERE  
+        </Box>
+      </div>
+
+        
     </div>
   );
 }
