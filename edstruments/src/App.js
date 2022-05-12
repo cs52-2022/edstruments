cd876a2013074014f0044e4e6ad6c74931673858
ulofreitas <83433693+ulofreitas@users.noreply.github.com>
2022-05-12 11:00:05 -0700

    added the other navbar options to match our hifi

--- edstruments/src/App.js
+++ edstruments/src/App.js
@@ -14,8 +14,13 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link href="#dashboard">Dashboard</Nav.Link>
+              <Nav.Link href="#resources">Resources</Nav.Link>
+              <Nav.Link href="#budgets">Budgets</Nav.Link>
+              <Nav.Link href="#transactions/pos">Transactions/POs</Nav.Link>
+              <Nav.Link href="#personnel">Personnel</Nav.Link>
+              <Nav.Link href="#schools">Schools/POs</Nav.Link>
+              <Nav.Link href="#assets">Assets</Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
